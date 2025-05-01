04dccd9d735ba83670b5b74ce8af4f730c3ed9f5
Bob.Boss <gitcourse@mobilo24.eu>
2025-05-01 19:09:40 +0000

    Correcting error in test

--- py-upper/test_uppercase.py
+++ py-upper/test_uppercase.py
@@ -2,7 +2,7 @@ import pytest
 from uppercase import to_uppercase
 
 def test_to_uppercase():
-    assert to_uppercase("hello") == "HELLOoo"
+    assert to_uppercase("hello") == "HELLO"
     assert to_uppercase("Python") == "PYTHON"
     assert to_uppercase("123abc") == "123ABC"
     assert to_uppercase("") == ""  # Test pustego napisu
