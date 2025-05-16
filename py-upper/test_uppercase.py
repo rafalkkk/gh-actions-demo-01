855475df7ea76e41be4beebbfb9553797056b2a7
Bob.Boss <gitcourse@mobilo24.eu>
2025-05-16 20:40:30 +0000

    Updating tests

--- py-upper/test_uppercase.py
+++ py-upper/test_uppercase.py
@@ -1,11 +1,11 @@
 import pytest
-from uppercase import to_uppercase
+from uppercase import to_upper
 
 def test_to_uppercase():
-    assert to_uppercase("hello") == "HELLO"
-    assert to_uppercase("Python") == "PYTHON"
-    assert to_uppercase("123abc") == "123ABC"
-    assert to_uppercase("") == ""  # Test pustego napisu
+    assert to_upper("hello") == "HELLO"
+    assert to_upper("Python") == "PYTHON"
+    assert to_upper("123abc") == "123ABC"
+    assert to_upper("") == ""  # Test pustego napisu
 
 if __name__ == "__main__":
     pytest.main()
